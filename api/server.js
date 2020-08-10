6f51a36813f2d92fcfd7fbdb9fbe2f0912abadaf
Khalil <khalilfoulks@gmail.com>
2020-08-10 16:38:05 -0400

    finished endpoints

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const AccountsRouter = require("../accounts/Accounts-router")
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`<h2> Node db 1 Project is UP! </h2>`);
+})
+
+server.use("/api/accounts", AccountsRouter)
 
 module.exports = server;
